4857ad01f6c9e0a44e039704dedc3214fe484eb2
maria-maia <maria.maia@sptech.school>
2026-02-23 19:07:09 -0300

    Atualizando rotas para cadastro de funcionário

--- web/src/controllers/cadastroFuncionarioController.js
+++ web/src/controllers/cadastroFuncionarioController.js
@@ -1,44 +1,48 @@
 var cadastroFuncionarioModel = require("../models/cadastroFuncionarioModel");
- 
+
 function cadastrar(req, res) {
-    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
-    var nome = req.body.nomeServer;
-    var email = req.body.emailServer;
-    var senha = req.body.senhaServer;
-    var cpf = req.body.cpfServer;
-    var idAdm = req.body.idAdmServer;
-    var idEmpresa = req.body.empresaServer;
+  // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+  var nome = req.body.nomeServer;
+  var email = req.body.emailServer;
+  var senha = req.body.senhaServer;
+  var cpf = req.body.cpfServer;
+  var nvlAcesso = req.body.nvlAcessoServer;
+  var idAdm = req.body.idAdmServer;
+  var idEmpresa = req.body.empresaServer;
 
-    // Faça as validações dos valores
-    if (nome == undefined) {
-        res.status(400).send("Seu nome está undefined!");
-    } else if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
-    } else if (senha == undefined) {
-        res.status(400).send("Sua senha está undefined!");
-    } else if (cpf == undefined) {
-        res.status(400).send("Seu token está undefined!");
-    } else {
-
-        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, cpf, idAdm, idEmpresa)
-            .then(
-                function (resultado) {
-                    res.json(resultado);
-                }
-            ).catch(
-                function (erro) {
-                    console.log(erro);
-                    console.log(
-                        "\nHouve um erro ao realizar o cadastro! Erro: ",
-                        erro.sqlMessage
-                    );
-                    res.status(500).json(erro.sqlMessage);
-                }
-            );
-    }
+  // Faça as validações dos valores
+  if (nome == undefined) {
+    res.status(400).send("Seu nome está undefined!");
+  } else if (email == undefined) {
+    res.status(400).send("Seu email está undefined!");
+  } else if (senha == undefined) {
+    res.status(400).send("Sua senha está undefined!");
+  } else if (cpf == undefined) {
+    res.status(400).send("Seu token está undefined!");
+  } else if (nvlAcesso == undefined) {
+    res.status(400).send("Seu token está undefined!");
+  } else if (idAdm == undefined) {
+    res.status(400).send("Seu token está undefined!");
+  } else if (idEmpresa == undefined) {
+    res.status(400).send("Seu token está undefined!");
+  } else {
+    // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+    cadastroFuncionarioModel
+      .cadastrar(nome, email, senha, cpf, nvlAcesso, idAdm, idEmpresa)
+      .then(function (resultado) {
+        res.json(resultado);
+      })
+      .catch(function (erro) {
+        console.log(erro);
+        console.log(
+          "\nHouve um erro ao realizar o cadastro! Erro: ",
+          erro.sqlMessage,
+        );
+        res.status(500).json(erro.sqlMessage);
+      });
+  }
 }
 
 module.exports = {
-    cadastrar
-}
+  cadastrar,
+};
