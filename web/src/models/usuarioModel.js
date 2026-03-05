c724dff4dc2e31ab66346f014368dba598c9d433
gustavoVbdr <gustavo.rodrigues@sptech.school>
2026-03-05 13:20:30 -0300

    Index link com login, fk_empresa login e back, alguns deletes

--- web/src/models/usuarioModel.js
+++ web/src/models/usuarioModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config");
 function autenticaremail(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucaoSql = `
-        SELECT id_usuario, nome, email, cpf, senha, fk_empresa as empresaId FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+        SELECT id_usuario, nome, email, cpf, senha, fk_empresa FROM usuario WHERE email = '${email}' AND senha = '${senha}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
@@ -12,20 +12,7 @@ function autenticaremail(email, senha) {
 function autenticarcpf(cpf, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", cpf, senha)
     var instrucaoSql = `
-        SELECT id_usuario, nome, email, cpf, senha, fk_empresa as empresaId FROM usuario WHERE cpf = '${cpf}' AND senha = '${senha}';
-    `;
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
-}
-
-// Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
-function cadastrar(nome, email, senha, fk_token) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, fk_token);
-    
-    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
-    //  e na ordem de inserção dos dados.
-    var instrucaoSql = `
-        INSERT INTO usuario (nome, email, senha, fk_token) VALUES ('${nome}', '${email}', '${senha}', '${fk_token}');
+        SELECT id_usuario, nome, email, cpf, senha, fk_empresa FROM usuario WHERE cpf = '${cpf}' AND senha = '${senha}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
@@ -33,6 +20,5 @@ function cadastrar(nome, email, senha, fk_token) {
 
 module.exports = {
     autenticaremail,
-    autenticarcpf,
-    cadastrar
+    autenticarcpf
 };
