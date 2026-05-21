daa38784c28cfc0e680af6084cfc64a37d23662e
04Richard07 <richard.araujo@sptech.school>
2026-05-21 17:34:42 -0300

    BackEnd Individual

--- web/src/routes/s3.js
+++ web/src/routes/s3.js
@@ -12,6 +12,7 @@ const s3 = new S3Client({
     }
 });
 
+// Rota da Isabela
 router.get("/client", async (req, res) => {
     try {
 
@@ -21,18 +22,35 @@ router.get("/client", async (req, res) => {
         });
 
         const resposta = await s3.send(comando);
-
         const dados = await resposta.Body.transformToString();
-
         res.json(JSON.parse(dados));
 
     } catch (erro) {
 
         console.error("Erro S3:", erro);
+        res.status(500).json({ erro: "Erro ao buscar dados do S3" });
 
-        res.status(500).json({
-            erro: "Erro ao buscar dados do S3"
+    }
+});
+
+// Rota do Richard  
+router.get("/richard", async (req, res) => {
+    try {
+
+        const comando = new GetObjectCommand({
+            Bucket: process.env.AWS_BUCKET_NAME,
+            Key: "CLIENT/richard.json"
         });
+
+        const resposta = await s3.send(comando);
+        const dados = await resposta.Body.transformToString();
+        res.json(JSON.parse(dados));
+
+    } catch (erro) {
+
+        console.error("Erro S3:", erro);
+        res.status(500).json({ erro: "Erro ao buscar dados do S3" });
+
     }
 });
 
