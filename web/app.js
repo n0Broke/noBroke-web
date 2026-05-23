e55da17638d0e9ceff65aa18874963e8167bf974
IsabelaTeixeiraRodrigues <isabela.rodrigues@sptech.school>
2026-05-23 01:52:38 -0300

    atualizando erro grafico status ordens

--- web/app.js
+++ web/app.js
@@ -6,7 +6,7 @@ var caminho_env = ambiente_processo === 'producao' ? '.env' : '.env.dev';
 require("dotenv").config({ path: caminho_env });
 
 var express = require("express");
-var cors = require("cors");
+var cors = require("cors")
 var path = require("path");
 var PORTA_APP = process.env.APP_PORT;
 var HOST_APP = process.env.APP_HOST;
